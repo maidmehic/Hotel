5fdbbaeb47769ec24d4f6bb2dc292ca9fd7751e2
Faruk <faruk.kardovic@edu.fit.ba>
2017-12-22 11:10:56 +0100

    test

--- Hotel.Data/Models/Drzava.cs
+++ Hotel.Data/Models/Drzava.cs
@@ -12,6 +12,6 @@ namespace Hotel.Data.Models
         // vise gradova
 
 
-        public virtual List<Grad> Grad { get; set; }
+        public virtual List<Grad> Grad { get; set; }//blabla
     }
 }
