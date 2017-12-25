af14716c116e26d75c89fd72bdbc5f3beaded8f1
Maid Mehić <maid.mehic@edu.fit.ba>
2017-12-25 18:29:55 +0100

    testna

--- Hotel.Data/Models/Grad.cs
+++ Hotel.Data/Models/Grad.cs
@@ -19,6 +19,6 @@ namespace Hotel.Data.Models
 
         // vise zaposlenika
 
-        public virtual List<Zaposlenik> Zaposlenik { get; set; }
+        public virtual List<Zaposlenik> Zaposlenik { get; set; }//test
     }
 }
